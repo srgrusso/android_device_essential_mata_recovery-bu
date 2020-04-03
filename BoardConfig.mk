362a29654bbc0a70e0412a8dcfc09c2547a5a4d5
srgrusso <srgrusso240@gmail.com>
2020-04-02 19:23:02 -0500

    Remove TARGET_HAS_NO_SELECT_BUTTON

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,6 @@ TARGET_BOOTLOADER_BOARD_NAME := Mata
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msm8998
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno540
-TARGET_HAS_NO_SELECT_BUTTON := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=mata user_debug=31 ehci-hcd.park=3
